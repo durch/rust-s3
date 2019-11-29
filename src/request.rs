6a6bc71e4e25faf2a3012589466c257c61455572
Drazen Urch <drazen@urch.eu>
2019-11-29 21:34:12 +0100

    Use virtual path style exclusivly

--- src/request.rs
+++ src/request.rs
@@ -51,16 +51,17 @@ impl<'a> Request<'a> {
     }
 
     fn url(&self) -> Url {
-        let mut url_str = match self.command {
-            Command::ListBucket { .. } => {
-                format!("{}://{}", self.bucket.scheme(), self.bucket.host())
-            }
-            _ => format!("{}://{}", self.bucket.scheme(), self.bucket.self_host()),
-        };
-        if let Command::ListBucket { .. } = self.command {
-            url_str.push_str("/");
-            url_str.push_str(&self.bucket.name());
-        }
+        // let mut url_str = match self.command {
+        //     Command::ListBucket { .. } => {
+        //         format!("{}://{}", self.bucket.scheme(), self.bucket.host())
+        //     }
+        //     _ => format!("{}://{}", self.bucket.scheme(), self.bucket.self_host()),
+        // };
+        let mut url_str = format!("{}://{}", self.bucket.scheme(), self.bucket.self_host());
+        // if let Command::ListBucket { .. } = self.command {
+        //     url_str.push_str("/");
+        //     url_str.push_str(&self.bucket.name());
+        // }
         if !self.path.starts_with('/') {
             url_str.push_str("/");
         }
@@ -190,12 +191,13 @@ impl<'a> Request<'a> {
             .iter()
             .map(|(k, v)| Ok((k.parse::<HeaderName>()?, v.parse::<HeaderValue>()?)))
             .collect::<Result<HeaderMap, S3Error>>()?;
-        match self.command {
-            Command::ListBucket { .. } => {
-                headers.insert(header::HOST, self.bucket.host().parse()?)
-            }
-            _ => headers.insert(header::HOST, self.bucket.self_host().parse()?),
-        };
+        // match self.command {
+        //     Command::ListBucket { .. } => {
+        //         headers.insert(header::HOST, self.bucket.host().parse()?)
+        //     }
+        //     _ => headers.insert(header::HOST, self.bucket.self_host().parse()?),
+        // };
+        headers.insert(header::HOST, self.bucket.self_host().parse()?);
         headers.insert(
             header::CONTENT_LENGTH,
             self.content_length().to_string().parse()?,
