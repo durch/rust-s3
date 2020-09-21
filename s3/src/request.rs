c6a2009302edb0bfc29b0a1f72dec43f272b2270
paul <54645365+koplas@users.noreply.github.com>
2020-09-21 13:06:27 +0200

    Make openssl optional

--- s3/src/request.rs
+++ s3/src/request.rs
@@ -493,10 +493,17 @@ impl<'a> Request<'a> {
         };
 
         let client = if cfg!(feature = "no-verify-ssl") {
-            Client::builder()
-                .danger_accept_invalid_certs(true)
-                .danger_accept_invalid_hostnames(true)
-                .build()
+            let client = Client::builder().danger_accept_invalid_certs(true);
+
+                cfg_if::cfg_if! {
+                    if #[cfg(feature = "no-verify-ssl")]
+                    {
+                        client.danger_accept_invalid_hostnames(true);
+                    }
+
+                }
+
+                client.build()
                 .expect("Could not build dangerous client!")
         } else {
             Client::new()
