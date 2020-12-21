b6c1b556de211d9f74f3b39d2698e98156aede67
Gleb Pomykalov <gleb@exogress.com>
2020-12-21 11:20:23 +0300

    fix surf

--- s3/src/surf_request.rs
+++ s3/src/surf_request.rs
@@ -1,5 +1,4 @@
 use async_std::io::ReadExt;
-use std::collections::HashMap;
 use std::io::Write;
 
 use super::bucket::Bucket;
@@ -59,10 +58,10 @@ impl<'a> Request for SurfRequest<'a> {
 
         let mut request = request.body(self.request_body());
 
-        for (name, value) in headers {
+        for (name, value) in headers.iter() {
             request = request.header(
-                HeaderName::from_bytes(name.as_bytes().to_vec()).unwrap(),
-                HeaderValue::from_bytes(value.as_bytes().to_vec()).unwrap(),
+                HeaderName::from_bytes(AsRef::<[u8]>::as_ref(&name).to_vec()).unwrap(),
+                HeaderValue::from_bytes(AsRef::<[u8]>::as_ref(&value).to_vec()).unwrap(),
             );
         }
 
@@ -109,10 +108,11 @@ impl<'a> Request for SurfRequest<'a> {
         let mut header_map = HeaderMap::new();
         let response = self.response().await?;
         let status_code = response.status();
+
         for (name, value) in response.iter() {
             header_map.insert(
-                name.to_string().to_ascii_lowercase(),
-                value.as_str().to_string(),
+                http::header::HeaderName::from_lowercase(name.to_string().to_ascii_lowercase().as_ref()).unwrap(),
+                value.as_str().parse().unwrap(),
             );
         }
         Ok((header_map, status_code.into()))
