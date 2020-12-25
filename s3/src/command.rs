a80044cfa2cd763c157ac8eb7079e8c223bece80
Simon THOBY <git@nightmared.fr>
2020-12-25 19:42:41 +0100

    add custom headers on put_object requests
    Useful to add metadata when uploading an object

--- s3/src/command.rs
+++ s3/src/command.rs
@@ -64,6 +64,7 @@ pub enum Command<'a> {
         content: &'a [u8],
         content_type: &'a str,
         multipart: Option<Multipart<'a>>,
+        custom_headers: Option<HeaderMap>,
     },
     PutObjectTagging {
         tags: &'a str,
@@ -89,7 +90,9 @@ pub enum Command<'a> {
         expiry_secs: u32,
         custom_headers: Option<HeaderMap>,
     },
-    InitiateMultipartUpload,
+    InitiateMultipartUpload {
+        custom_headers: Option<HeaderMap>,
+    },
     UploadPart {
         part_number: u32,
         content: &'a [u8],
@@ -127,7 +130,7 @@ impl<'a> Command<'a> {
             | Command::DeleteObjectTagging
             | Command::AbortMultipartUpload { .. }
             | Command::DeleteBucket => HttpMethod::Delete,
-            Command::InitiateMultipartUpload | Command::CompleteMultipartUpload { .. } => {
+            Command::InitiateMultipartUpload { .. } | Command::CompleteMultipartUpload { .. } => {
                 HttpMethod::Post
             }
             Command::HeadObject => HttpMethod::Head,
