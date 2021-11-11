fbcfc1b1fb64ac133a4fef60ef4cd5ece7599072
Riley <asonix@asonix.dog>
2021-11-11 06:00:31 -0500

    Make attohttpc optional for credentials (#231)
    * Make attohttpc optional for credentials
    
    * Enabling tls implies enabling http

--- aws-creds/src/credentials.rs
+++ aws-creds/src/credentials.rs
@@ -124,6 +124,7 @@ static REQUEST_TIMEOUT_MS: AtomicU32 = AtomicU32::new(0);
 /// Short durations are bumped to one millisecond, and durations
 /// greater than 4 billion milliseconds (49 days) are rounded up to
 /// infinity (no timeout).
+#[cfg(feature = "http-credentials")]
 pub fn set_request_timeout(timeout: Option<Duration>) {
     use std::convert::TryInto;
     let duration_ms = timeout
@@ -138,6 +139,7 @@ pub fn set_request_timeout(timeout: Option<Duration>) {
 }
 
 /// Sends a GET request to `url` with a request timeout if one was set.
+#[cfg(feature = "http-credentials")]
 fn http_get(url: &str) -> attohttpc::Result<attohttpc::Response> {
     let mut builder = attohttpc::get(url);
 
@@ -150,6 +152,7 @@ fn http_get(url: &str) -> attohttpc::Result<attohttpc::Response> {
 }
 
 impl Credentials {
+    #[cfg(feature = "http-credentials")]
     pub fn from_sts_env(session_name: &str) -> Result<Credentials> {
         let role_arn = env::var("AWS_ROLE_ARN")?;
         let web_identity_token_file = env::var("AWS_WEB_IDENTITY_TOKEN_FILE")?;
@@ -157,6 +160,7 @@ impl Credentials {
         Credentials::from_sts(&role_arn, session_name, &web_identity_token)
     }
 
+    #[cfg(feature = "http-credentials")]
     pub fn from_sts(
         role_arn: &str,
         session_name: &str,
@@ -200,6 +204,7 @@ impl Credentials {
         })
     }
 
+    #[cfg(feature = "http-credentials")]
     pub fn default() -> Result<Credentials> {
         Credentials::new(None, None, None, None, None)
     }
@@ -215,6 +220,7 @@ impl Credentials {
 
     /// Initialize Credentials directly with key ID, secret key, and optional
     /// token.
+    #[cfg(feature = "http-credentials")]
     pub fn new(
         access_key: Option<&str>,
         secret_key: Option<&str>,
@@ -260,6 +266,7 @@ impl Credentials {
         Credentials::from_env_specific(None, None, None, None)
     }
 
+    #[cfg(feature = "http-credentials")]
     pub fn from_instance_metadata() -> Result<Credentials> {
         #[derive(Deserialize)]
         #[serde(rename_all = "PascalCase")]
