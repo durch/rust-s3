05e62b767dfc372277024978e2a9f7d4f9b2a90e
durch <durch@users.noreply.github.com>
2022-04-22 00:07:22 +0200

    awscreds 0.29.1

--- aws-creds/src/error.rs
+++ aws-creds/src/error.rs
@@ -10,8 +10,9 @@ pub enum CredentialsError {
     ConfigMissingAccessKeyId,
     #[error("Missing aws_access_key_id section in config")]
     ConfigMissingSecretKey,
-    #[error("Neither {0}, nor {1} exists in the environment",)]
+    #[error("Neither {0}, nor {1} exists in the environment")]
     MissingEnvVar(String, String),
+    #[cfg(feature = "http-credentials")]
     #[error("attohttpc: {0}")]
     Atto(#[from] attohttpc::Error),
     #[error("ini: {0}")]
@@ -25,5 +26,5 @@ pub enum CredentialsError {
     #[error("env var: {0}")]
     Env(#[from] std::env::VarError),
     #[error("Invalid home dir")]
-    HomeDir
+    HomeDir,
 }
